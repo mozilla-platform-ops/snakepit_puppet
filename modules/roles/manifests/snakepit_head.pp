89f743ca74c47fb9c96bbde71070899c68755d4c
Andrew Erickson <aerickson@mozilla.com>
2021-11-09 14:38:17 -0800

    use treydock/slurm

--- modules/roles/manifests/snakepit_head.pp
+++ modules/roles/manifests/snakepit_head.pp
@@ -6,13 +6,26 @@ class roles::snakepit_head {
     # include ::roles_profiles::profiles::bitbar_devicepool
     # include ::roles_profiles::profiles::remove_bootstrap_user
 
-    class { 'munge':
-      # the 'key' defines the security realm for a node, https://github.com/dun/munge/wiki/Man-7-munge
-      munge_key_content => base64('decode', lookup('munge::munge_key_content'))
-    }
 
-    class { 'moz_slurm':
-      slurm_version => '21.08'
-    }
+    #
+    # moz_slurm route
+    #
+
+    # class { 'munge':
+    #   # the 'key' defines the security realm for a node, https://github.com/dun/munge/wiki/Man-7-munge
+    #   munge_key_content => base64('decode', lookup('munge::munge_key_content'))
+    # }
+
+    # class { 'moz_slurm':
+    #   slurm_version => '21.08'
+    # }
+
+    #
+    # treydock/slurm route
+    #
+
+    # TODO: install mysql/maria
+
+    include slurm
 
 }
