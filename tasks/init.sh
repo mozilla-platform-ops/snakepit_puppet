6e6fad87aa154b1f508232eaaabad98188b39f80
Andrew Erickson <aerickson@mozilla.com>
2021-11-10 17:45:08 -0800

    fixing pre-commit issues

--- tasks/init.sh
+++ tasks/init.sh
@@ -1,2 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
 # Example file in which task code would be written, note the corresponding init.json file where task metadata would be defined
 # Docs Reference: https://puppet.com/docs/bolt/latest/writing_tasks_and_plans.html#plans
