c373831072dcc1009af09bad5b414404775ccfb4
Andrew Erickson <aerickson@mozilla.com>
2021-11-11 14:09:05 -0800

    add more spec tests

--- test/integration/snakepit-head/inspec/services_spec.rb
+++ test/integration/snakepit-head/inspec/services_spec.rb
@@ -1,4 +1,28 @@
+describe service('munge') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
 describe service('slurmctld') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('slurmdbd') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('mysql') do
+  it { should be_installed }
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe service('slurmrestd') do
   it { should be_installed }
   it { should be_enabled }
   it { should be_running }
