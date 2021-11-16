4fa2b09f3854e2b7ce299e24271d9081c112dff7
Andrew Erickson <aerickson@mozilla.com>
2021-11-15 16:05:46 -0800

    increase timeout for spack installing lmod and singularity

--- modules/moz_slurm/manifests/spack.pp
+++ modules/moz_slurm/manifests/spack.pp
@@ -36,6 +36,7 @@ class moz_slurm::spack {
     provider => shell,
     user     => 'slurm',
     unless   => "${spack_bin_path} find lmod",
+    timeout  => 3600,
   }
 
   file_line { 'add lmod env source to .bashrc':
@@ -49,5 +50,18 @@ class moz_slurm::spack {
     provider => shell,
     user     => 'slurm',
     unless   => "${spack_bin_path} find singularity",
+    timeout  => 3600,
   }
+
+  # TODO: define a set of modules all users get
+  #       https://lmod.readthedocs.io/en/latest/070_standard_modules.html
+
+  # TODO: make loading module easier? currently need fully hashed name...
+  #       - like 'module load singularity-3.8.3-gcc-7.5.0-pyi4mc6'
+  #       https://github.com/spack/spack/discussions/13775
+  #       https://spack.readthedocs.io/en/latest/config_yaml.html
+
+  # for docs: how to use spack in scripts:
+  #   https://spack.readthedocs.io/en/latest/module_file_support.html#modules
+
 }
