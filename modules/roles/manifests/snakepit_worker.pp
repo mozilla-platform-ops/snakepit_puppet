3c27fd70ea5bd41b41a425db5c3bfa9757bff514
Andrew Erickson <aerickson@mozilla.com>
2021-11-18 14:18:46 -0800

    todo wrt bolt to install singularity and lmod

--- modules/roles/manifests/snakepit_worker.pp
+++ modules/roles/manifests/snakepit_worker.pp
@@ -18,9 +18,11 @@ class roles::snakepit_worker {
 
     # install spack
     include moz_slurm::spack
+
     # install lmod and spack
     # disabled for now, manually run on worker nodes for now. maybe use hiera value to control later?
     # include moz_slurm::spack_lmod
     # include moz_slurm::spack_singularity
+    # TODO: figure out how to call the two above modules from bolt as a post-convergence step
 
 }
