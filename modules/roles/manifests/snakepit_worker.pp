f64a9e3fc8ecf2dbd44dc248143dd410d610aab8
Andrew Erickson <aerickson@mozilla.com>
2021-12-07 23:28:58 -0800

    worker role: cleanup

--- modules/roles/manifests/snakepit_worker.pp
+++ modules/roles/manifests/snakepit_worker.pp
@@ -19,16 +19,12 @@ class roles::snakepit_worker {
     include moz_slurm::spack
 
     # steps below moved to roles::snakepit_worker_post to be run by bolt post-convergence
-
+    #
     # install lmod and spack
-    # disabled for now (takes super long)
-    # - manually run on worker nodes for now. maybe use hiera value to control later?
-    # TODO: write bolt plan that runs above as a post-convergence step
     # include moz_slurm::spack_lmod
     # include moz_slurm::spack_singularity
-
+    #
     # manage packages (including cuda/nvidia-driver)
-    # TODO: move this out of initial convergence? see above
     # include moz_slurm::worker::install_cuda
 
 }
