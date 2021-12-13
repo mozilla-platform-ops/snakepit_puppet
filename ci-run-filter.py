1536b04744490c516a0dd85689261dfda63a42fd
Andrew Erickson <aerickson@mozilla.com>
2021-12-13 15:45:50 -0800

    ci-run: add notes

--- ci-run-filter.py
+++ ci-run-filter.py
@@ -5,30 +5,33 @@ import os
 import sys
 import subprocess
 
-# args:
-#  - substring: string to check for (file ending like'.pp' or path like 'modules/')
-#  - command: the command to run if above is matched
-#  - optional: base: base branch if not master or main
-
-# https://stackoverflow.com/questions/5243593/how-to-trigger-a-build-only-if-changes-happen-on-particular-set-of-files
+# ci-run-filter.py
+#
+#   only runs the given command (usually a long running test command) if the
+#   files changed in current git commit match the pattern specified.
+#
+#   exceptions:
+#     - always runs on master/main
+#     - TODO: run or don't run based on certain tags in the commit message
+#
 
 # advantages:
 # - saves a bunch of time if commits are just .txt files changes
+#
 # downsides:
-# - if a test passes, have to check to see that this step was run vs knowing
-#   - make the CI system show skipped vs passed? not possible in CircleCI
+# - if patterns miss dependencies, failures could be missed
+#   - suggest full path matches (/path/to/module) vs suffixes ('.pp')
+# - if a test passes, users to check to see that this step was run vs knowing
+#   - possible solution: let CI system know by exiting a specific code.
+#     - circleci: https://ideas.circleci.com/cloud-feature-requests/p/set-job-state-based-on-exit-code
+#     - gitlab: https://gitlab.com/gitlab-org/gitlab/-/issues/16733
+#     - github: no public issue mentioning this
 
-# logic:
-# - if branch == main|master
-#   - run
-# - if files of type x have changed (git diff-tree --name-only HEAD)
-#   - run
-# - if commit message has FORCE_TRIGGER or similar keyword
-#   - run
-# - else
-#   - print "no files matching pattern and not on master/main/base branch"
-#   - ideally set state 'skipped' if ci system supported
-#   - exit 0
+
+def inspect_commit_message():
+    # `git show HEAD -s` and look for SKIP_CI or FORCE_CI
+    # - see what CI services are using for defaults
+    pass
 
 
 def files_in_diff_match(match_string, verbose=False):
@@ -39,6 +42,7 @@ def files_in_diff_match(match_string, verbose=False):
     for line in output.split("\n")[1:]:
         if verbose:
             print("  %s" % line)
+        # TODO: iterate over array of potential matches
         if match_string in line:
             print("'%s' matched pattern, running test..." % line)
             return True
@@ -48,8 +52,10 @@ def files_in_diff_match(match_string, verbose=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument('--command', '-c')
+    # TODO: take multiple
     parser.add_argument('--substring_to_match', '-s')
     parser.add_argument('--verbose', '-v', default=False, action='store_true')
+    # TODO: add skip_commit_message, run_commit_message
     args = parser.parse_args()
     print(args)
 
