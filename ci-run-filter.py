a3cd37f8f08a7f8bffe84775cc24f32fe855bfcd
Andrew Erickson <aerickson@mozilla.com>
2021-12-13 19:09:38 -0800

    ci-run-filter: refactoring, use configparser

--- ci-run-filter.py
+++ ci-run-filter.py
@@ -4,6 +4,7 @@ import argparse
 import os
 import sys
 import subprocess
+import configparser
 
 # ci-run-filter.py
 #
@@ -43,7 +44,7 @@ def inspect_commit_message():
     pass
 
 
-def files_in_diff_match(match_strings, verbose=False, git_ref='HEAD'):
+def files_in_diff_match(match_strings, verbose=False, git_ref="HEAD"):
     # doesn't get full path
     # cmd_to_run = "git diff-tree --name-only %s 2>&1" % git_ref
 
@@ -66,15 +67,9 @@ def files_in_diff_match(match_strings, verbose=False, git_ref='HEAD'):
     return False
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument('--command', '-c')
-    parser.add_argument('--substring_to_match', '-s', dest='match_strings', action='append')
-    parser.add_argument('--verbose', '-v', default=False, action='store_true')
-    parser.add_argument('--git-ref', '-g', dest="git_ref", default='HEAD')
-    # TODO: add skip_commit_message, run_commit_message
-    args = parser.parse_args()
-
+def run_test_if_matches(
+    match_strings, command, git_ref, force_run, force_skip, verbose=False
+):
     file_name = os.path.basename(__file__)
 
     # debugging
@@ -93,26 +88,131 @@ if __name__ == "__main__":
     # TODO: what order/priority for force options?
     if branch == "master" or branch == "main":
         pass
-    if files_in_diff_match(args.match_strings, git_ref=args.git_ref, verbose=args.verbose):
+    if files_in_diff_match(match_strings, git_ref=git_ref, verbose=verbose):
         pass
     else:
         # TODO: make sure this matches actual order above? or just omit it?
         print("- No changed files matched the selection criteria.")
         print("- Not on master or main braches.")
         # print("- Commit doesn't have force_run or force_skip keywords in message.")
-        print("Skipping test.")
+        print("%s: Skipping test." % file_name)
         sys.exit(0)
 
     # run test
-    if args.verbose:
-        print("%s: running command ('%s')..." % (file_name, args.command))
-    process = subprocess.Popen(args.command, stdout=subprocess.PIPE, shell=True)
-    for c in iter(lambda: process.stdout.read(1), b''):
+    if verbose:
+        print("%s: running command ('%s')..." % (file_name, command))
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
+    for c in iter(lambda: process.stdout.read(1), b""):
         sys.stdout.buffer.write(c)
         # flush buffer to avoid hiding output
         sys.stdout.buffer.flush()
 
     # collect the exit code
     process.wait()
-    print('%s: process exited with result code %s' % (file_name, process.returncode))
+    print("%s: process exited with result code %s" % (file_name, process.returncode))
     sys.exit(process.returncode)
+
+
+def main(argv=None):
+    # TODO: consider requiring configargparse (https://pypi.org/project/ConfigArgParse/)
+    # - makes it easier to handle config files, arguments, env vars, etc
+
+    # Do argv default this way, as doing it in the functional
+    # declaration sets it at compile time.
+    if argv is None:
+        argv = sys.argv
+
+    # Parse any conf_file specification
+    # We make this parser with add_help=False so that
+    # it doesn't parse -h and print help.
+    conf_parser = argparse.ArgumentParser(
+        description=__doc__,  # printed with -h/--help
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        # Turn off help, so we print all options in response to -h
+        add_help=False,
+    )
+    # fully hidden with argparse.SUPPRESS
+    conf_parser.add_argument(
+        "--conf_file",
+        # help="The config file to use.",
+        help=argparse.SUPPRESS,
+        default=".ci-run-filter.cfg",
+        metavar="FILE",
+    )
+    args, remaining_argv = conf_parser.parse_known_args()
+
+    defaults = {  # "option":"default",
+        "force-run": "CI_FORCE_RUN",
+        "force-skip": "CI_FORCE_SKIP",
+    }
+
+    if os.path.exists(args.conf_file):
+        config = configparser.ConfigParser()
+        try:
+            config.read([args.conf_file])
+            defaults.update(dict(config.items("main")))
+        except configparser.NoSectionError:
+            # TODO: warn?
+            pass
+        # TODO: handle loading filters (manually)
+
+    # Parse rest of arguments
+    # Don't suppress add_help here so it will handle -h
+    parser = argparse.ArgumentParser(
+        # Inherit options from config_parser
+        parents=[conf_parser]
+    )
+    parser.set_defaults(**defaults)
+    parser.add_argument("--verbose", "-v", action="store_true")
+
+    main_group = parser.add_argument_group("main")
+    secondary_group = parser.add_argument_group("advanced")
+    # parser.add_argument("--option")  # test option
+    main_group.add_argument("--command", "-c", required=True)
+    main_group.add_argument(
+        "--substring_to_match",
+        "-s",
+        dest="match_strings",
+        action="append",
+        required=True,
+    )
+    secondary_group.add_argument(
+        "--git-ref",
+        dest="git_ref",
+        default="HEAD",
+        help="For testing, use a commit other than HEAD.",
+    )
+    secondary_group.add_argument(
+        "--force-run",
+        default=defaults["force-run"],
+        help="String that forces a run of the command if found in the commit message.",
+    )
+    secondary_group.add_argument(
+        "--force-skip",
+        default=defaults["force-skip"],
+        help="String that force skips a run of the command if found in the commit message.",
+    )
+
+    args = parser.parse_args(remaining_argv)
+    # print(args)
+    # print("Option is \"{}\"".format(args.option))
+    # return(0)
+    # print(args.force_skip)
+    # sys.exit(1)
+
+    run_test_if_matches(
+        args.match_strings,
+        args.command,
+        args.git_ref,
+        args.force_run,
+        args.force_skip,
+        verbose=args.verbose,
+    )
+
+    # shouldn't be here, throw error if we are
+    return 1
+
+
+if __name__ == "__main__":
+    sys.exit(main())
