0b2089b73ef4e8847fb8e45f227e4a3d810a29c5
Andrew Erickson <aerickson@mozilla.com>
2022-01-06 15:49:08 -0500

    singularity: see if cwd was issue

--- modules/moz_slurm/manifests/spack_singularity.pp
+++ modules/moz_slurm/manifests/spack_singularity.pp
@@ -9,9 +9,11 @@ class moz_slurm::spack_singularity {
   # install singularity
   exec {'install singularity':
     # command  => ". /home/slurm/software/spack/share/spack/setup-env.sh && ${spack_bin_path} install singularity",
-    command  => "${spack_bin_path} install singularity",
-    provider => bash,
+    # command  => "${spack_bin_path} install singularity",
+    command  => "bash -i -c '${spack_bin_path} install singularity'",
+    provider => shell,  # uses posix by default
     user     => 'slurm',
+    cwd      => '/home/slurm',
     unless   => "${spack_bin_path} find singularity",
     timeout  => 3600,
   }
