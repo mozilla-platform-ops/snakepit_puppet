bdbcdccc2b4bd59e880a090568998dc47eeab975
Andrew Erickson <aerickson@mozilla.com>
2022-01-06 18:44:45 -0500

    moz_slurm: create /opt/moz_slurm dir

--- modules/moz_slurm/manifests/init.pp
+++ modules/moz_slurm/manifests/init.pp
@@ -2,9 +2,16 @@ class moz_slurm {
 
   # include moz_slurm::spack_variables
 
-  file {'moz_slurm dir':
+  file {'/moz_slurm dir':
     ensure => 'directory',
     path   => '/moz_slurm',
+    owner  => 'slurm',
+    group  => 'slurm'
+  }
+
+  file {'/opt/moz_slurm dir':
+    ensure => 'directory',
+    path   => '/opt/moz_slurm',
     owner  => 'slurm',
     group  => 'slurm'
   }
