12c56a2d6159f3f5c57d265ea782601ed1a7a445
Andrew Erickson <aerickson@mozilla.com>
2022-01-06 16:40:09 -0500

    singularity: desperation mode

--- modules/moz_slurm/files/install_singularity.sh
+++ modules/moz_slurm/files/install_singularity.sh
@@ -2,9 +2,11 @@
 
 set -e
 
-source /home/slurm/software/spack/share/spack/setup-env.sh
-source "$(spack location -i lmod)/lmod/lmod/init/bash"
+. /home/slurm/software/spack/share/spack/setup-env.sh
+. "$(spack location -i lmod)/lmod/lmod/init/bash"
 
 env
+
+spack load go
 
 spack install singularity
