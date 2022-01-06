460b4f22c7c7f49056c4d4992613b57986acb730
Andrew Erickson <aerickson@mozilla.com>
2022-01-06 15:55:33 -0500

    sing: add spack find for debugging

--- modules/moz_slurm/manifests/spack_singularity.pp
+++ modules/moz_slurm/manifests/spack_singularity.pp
@@ -8,9 +8,9 @@ class moz_slurm::spack_singularity {
 
   # install singularity
   exec {'install singularity':
-    # command  => ". /home/slurm/software/spack/share/spack/setup-env.sh && ${spack_bin_path} install singularity",
+    command  => ". /home/slurm/software/spack/share/spack/setup-env.sh && ${spack_bin_path} find && ${spack_bin_path} install singularity",
     # command  => "${spack_bin_path} install singularity",
-    command  => "BASH_ENV=~/.bashrc bash -c '${spack_bin_path} install singularity'",
+    # command  => "BASH_ENV=~/.bashrc bash -c '${spack_bin_path} install singularity'",
     provider => shell,  # uses posix by default
     user     => 'slurm',
     cwd      => '/home/slurm',
