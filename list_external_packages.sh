bd0a1a12b9b87821ebf5ab928dbb17beb4ffe1ae
Andrew Erickson <aerickson@mozilla.com>
2022-01-10 18:51:02 -0500

    add note about lz4 version

--- list_external_packages.sh
+++ list_external_packages.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# from https://askubuntu.com/questions/342434/find-what-packages-are-installed-from-a-repository
+
 # Print packages installed from different origins.
 # Exclude standard Ubuntu repositories.
 
@@ -11,7 +13,9 @@ grep -H '^Origin:' /var/lib/apt/lists/*Release | grep -v ' Ubuntu$' | sort -u \
     echo "$origin":
 
     list=${line%%:*}
+    # original line, doesn't work on lz4 archives
     #sed -rn 's/^Package: (.*)$/\1/p' ${list%_*Release}*Packages | sort -u \
+    # updated for lz4
     lz4cat "${list%_*Release}"*Packages.lz4 | sed -rn 's/^Package: (.*)$/\1/p' | sort -u \
     | xargs -r dpkg -l 2>/dev/null | grep '^.i '
     echo
