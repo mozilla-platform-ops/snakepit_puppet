9f7224608a1ddfc67fbc7be7422dd57473d3144f
Andrew Erickson <aerickson@mozilla.com>
2022-01-24 19:12:27 -0500

    cleanup, ordering via chaining

--- modules/roles/manifests/slurm_worker.pp
+++ modules/roles/manifests/slurm_worker.pp
@@ -2,23 +2,18 @@ class roles::slurm_worker {
 
     # TODO: manage users and ssh keys
 
-    # munge is installed by slurm module
-    #
-    # class { 'munge':
-    #   # the 'key' defines the security realm for a node, https://github.com/dun/munge/wiki/Man-7-munge
-    #   munge_key_content => base64('decode', lookup('munge::munge_key_content'))
-    # }
-
-    # manage packages (including cuda/nvidia-driver)
-    include moz_slurm
-    include moz_slurm::worker::install_cuda
-
     include slurm
 
     # TODO: should moz_slurm's children include this?
+    include moz_slurm
     include moz_slurm::packages
     include moz_slurm::worker::nfs
     include moz_slurm::worker::disable_automated_upgrades
+    # manage packages (including cuda/nvidia-driver)
+    include moz_slurm::worker::install_cuda
+
+    # ensure that cuda packages are present before doing slurm installation
+    Class['moz_slurm::worker::install_cuda'] ~> Class['slurm']
 
     # TODO: configure worker's env vars to use proxy
     # include moz_slurm::worker::proxy_env
