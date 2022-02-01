b3d749fbd294cfb3610b80d65e888e682cd932d9
Andrew Erickson <aerickson@mozilla.com>
2022-01-31 17:35:00 -0800

    add comment

--- modules/moz_slurm/manifests/head/place_cuda.pp
+++ modules/moz_slurm/manifests/head/place_cuda.pp
@@ -1,6 +1,9 @@
 class moz_slurm::head::place_cuda {
 
   # place libcudnnn files on nfs share
+  #
+  # cuDNN v8.3.2 (January 10th, 2022), for CUDA 11.5
+  # https://developer.nvidia.com/rdp/cudnn-download#a-collapse832-115
   file { 'cudnn-local-repo-ubuntu1804-8.3.2.44_1.0-1_amd64.deb':
     ensure => 'present',
     path   => '/data/ro/data/relops_data/cudnn-local-repo-ubuntu1804-8.3.2.44_1.0-1_amd64.deb',
