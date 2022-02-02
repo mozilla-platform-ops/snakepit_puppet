822886d8e1bf808ee212d59de642f34420e1c862
Andrew Erickson <aerickson@mozilla.com>
2022-02-01 16:55:23 -0800

    finalize libcudnn install process

--- Puppetfile
+++ Puppetfile
@@ -4,28 +4,29 @@
 # The following directive installs modules to the managed moduledir.
 moduledir '.modules'
 
-mod 'puppet/prometheus', '12.3.0'
-mod 'herculesteam/augeasproviders_core', '3.1.0'
-mod 'puppetlabs/inifile', '5.2.0'
-mod 'treydock/slurm_providers', '0.12.3'
-mod 'puppet/alternatives', '3.0.0'
-mod 'puppet/archive', '6.0.1'
-mod 'puppet/systemd', '3.5.1'
-mod 'puppet/logrotate', '6.0.0'
-mod 'herculesteam/augeasproviders_sysctl', '2.6.2'
-mod 'puppet/epel', '3.1.0'
-mod 'puppetlabs/concat', '7.1.1'
-mod 'puppetlabs/firewall', '3.2.0'
-mod 'puppetlabs/mysql', '12.0.1'
-mod 'puppetlabs/facts', '1.4.0'
-mod 'puppetlabs/stdlib', '8.1.0'
-mod 'puppetlabs/vcsrepo', '5.0.0'
-mod 'service',
-  git: 'https://github.com/puppetlabs/puppetlabs-service',
-  ref: 'a819beee25352924b2c7f15dea2722ff2309bb46'
-mod 'slurm',
-  git: 'https://github.com/treydock/puppet-slurm',
-  ref: 'e1a4506018b8ddb490f68d0ec823da762a3b43c5'
+mod 'puppetlabs/apt', '8.3.0'
 mod 'munge',
   git: 'https://github.com/treydock/puppet-munge',
   ref: 'c9c0d914eebbbb78c77dd0b0d17c85414bd4a977'
+mod 'slurm',
+  git: 'https://github.com/treydock/puppet-slurm',
+  ref: 'e1a4506018b8ddb490f68d0ec823da762a3b43c5'
+mod 'service',
+  git: 'https://github.com/puppetlabs/puppetlabs-service',
+  ref: 'a819beee25352924b2c7f15dea2722ff2309bb46'
+mod 'puppetlabs/vcsrepo', '5.0.0'
+mod 'puppetlabs/stdlib', '8.1.0'
+mod 'puppetlabs/facts', '1.4.0'
+mod 'puppetlabs/mysql', '12.0.1'
+mod 'puppetlabs/firewall', '3.2.0'
+mod 'puppetlabs/concat', '7.1.1'
+mod 'puppet/epel', '3.1.0'
+mod 'herculesteam/augeasproviders_sysctl', '2.6.2'
+mod 'puppet/logrotate', '6.0.0'
+mod 'puppet/systemd', '3.5.1'
+mod 'puppet/archive', '6.0.1'
+mod 'puppet/alternatives', '3.0.0'
+mod 'treydock/slurm_providers', '0.12.3'
+mod 'puppetlabs/inifile', '5.2.0'
+mod 'herculesteam/augeasproviders_core', '3.1.0'
+mod 'puppet/prometheus', '12.3.0'
--- modules/moz_slurm/manifests/worker/install_libcudnn.pp
+++ modules/moz_slurm/manifests/worker/install_libcudnn.pp
@@ -1,20 +1,51 @@
 class moz_slurm::worker::install_libcudnn {
 
+  include apt
+
   # install libcudnn
+  #   - steps from https://docs.nvidia.com/deeplearning/cudnn/install-guide/index.html
   #   its a 1gb file, so shouldn't commit in repo.
   #
   # steps:
-  #   - store on aws s3 account
-  #   - cache in nfs shared area on master
-  #   - install via nfs mount
+  #   - store on aws s3 account (done once)
+  #   - cache in nfs shared area on master (done in moz_slurm::head::place_cuda)
+  #   - install via nfs mount (this class)
+  #
+  # notes:
+  #   - the s3 cached file still doesn't contain all required files.
+  #     - during apt installation, things are fetched from network :(
+  #       - the whole point of caching the file. UGH.
 
-  # placement on nfs share done in moz_slurm::head::place_cuda
+  $cudnn_package_root = 'cudnn-local-repo-ubuntu1804'
+  $cudnn_version = '8.3.2.44'
+  $cuda_version = '11.5'
+  # from `gpg --keyid-format 0xlong /var/cudnn-local-repo-ubuntu1804-8.3.2.44/7fa2af80.pub`
+  $cudnn_repo_apt_key_fingerprint = 'AE09FE4BBD223A84B2CCFCE3F60F4B3D7FA2AF80'  # pragma: allowlist secret
 
   # install from nfs mount
-  package { 'cudnn-local-repo-ubuntu1804-8.3.2.44':
+  $cudnn_repo_package = "${cudnn_package_root}-${cudnn_version}"
+  package { $cudnn_repo_package:
     ensure   => installed,
     provider => dpkg,
     source   => '/data/ro/data/relops_data/cudnn-local-repo-ubuntu1804-8.3.2.44_1.0-1_amd64.deb'
   }
+
+  # sudo apt-key add /var/cudnn-local-repo-*/7fa2af80.pub
+  apt::key { 'cudatools <cudatools@nvidia.com>':
+    id     =>  $cudnn_repo_apt_key_fingerprint,
+    source => "/var/cudnn-local-repo-ubuntu1804-${cudnn_version}/7fa2af80.pub",
+    notify => Exec['apt_update']
+  }
+
+  # Install the runtime library.
+  #   sudo apt-get install libcudnn8=8.x.x.x-1+cudaX.Y
+  # Install the developer library.
+  #   sudo apt-get install libcudnn8-dev=8.x.x.x-1+cudaX.Y
+  # Install the code samples and the cuDNN library documentation.
+  #   sudo apt-get install libcudnn8-samples=8.x.x.x-1+cudaX.Y
+  $cudnn_packages = [ "libcudnn8=${cudnn_version}-1+cuda${cuda_version}",
+                      "libcudnn8-dev=${cudnn_version}-1+${cuda_version}",
+                      "libcudnn8-samples=${cudnn_version}-1+${cuda_version}" ]
+  package { $cudnn_packages: ensure => 'installed' }
 
 }
