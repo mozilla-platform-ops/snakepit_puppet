4d2012b35165350634ceeae0f7b0117d6f352a20
Andrew Erickson <aerickson@mozilla.com>
2022-05-02 15:04:39 -0400

    nfs: not using udp option any longer

--- test/integration/snakepit-worker/inspec/nfs_spec.rb
+++ test/integration/snakepit-worker/inspec/nfs_spec.rb
@@ -14,7 +14,7 @@ end
 describe etc_fstab.where { mount_point == "/data/ro" } do
   its("device_name") { should cmp "192.168.1.1:/snakepit/shared" }
   its("file_system_type") { should cmp "nfs" }
-  its("mount_options") { should eq [["nosuid", "hard", "udp", "bg", "noatime"]] }
+  its("mount_options") { should eq [["nosuid", "hard", "bg", "noatime"]] }
   its("dump_options") { should cmp "0" }
   its("file_system_options") { should cmp "0" }
 end
@@ -22,7 +22,7 @@ end
 describe etc_fstab.where { mount_point == "/data/rw" } do
   its("device_name") { should cmp "192.168.1.1:/data/rw" }
   its("file_system_type") { should cmp "nfs" }
-  its("mount_options") { should eq [["nosuid", "hard", "udp", "bg", "noatime"]] }
+  its("mount_options") { should eq [["nosuid", "hard", "bg", "noatime"]] }
   its("dump_options") { should cmp "0" }
   its("file_system_options") { should cmp "0" }
 end
