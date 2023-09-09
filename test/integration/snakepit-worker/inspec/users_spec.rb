0ad046e8065cc16c87a00ceb647ea93e80e5f83a
Andrew Erickson <aerickson@mozilla.com>
2023-09-09 02:51:01 -0700

    tweak user spec

--- test/integration/snakepit-worker/inspec/users_spec.rb
+++ test/integration/snakepit-worker/inspec/users_spec.rb
@@ -1,27 +1,23 @@
-describe "users" do
-  describe user("root") do
-    it { should exist }
-  end
+describe user("root") do
+  it { should exist }
 end
 
 # TODO: check for slurm user and correct uid/gid
 #       ... slurm group
 
-describe "users" do
-  describe user("aerickson") do
-    it { should exist }
-    its('groups') { should eq ["users", "sudo", "slurm", "admin"]}
-  end
+describe user("aerickson") do
+  it { should exist }
+  its('groups') { should eq ["users", "sudo", "slurm", "admin"]}
+end
 
-  describe user("epavlov") do
-    it { should exist }
-    its('groups') { should eq ['users', 'slurm']}
-  end
+describe user("epavlov") do
+  it { should exist }
+  its('groups') { should eq ['users', 'slurm']}
+end
 
-  describe user("gtatum") do
-    it { should exist }
-    its('groups') { should eq ['users', 'slurm']}
-  end
+describe user("gtatum") do
+  it { should exist }
+  its('groups') { should eq ['users', 'slurm']}
 end
 
 describe file('/etc/sudoers') do
