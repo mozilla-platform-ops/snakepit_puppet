637ac863ba77e95d1a5b32b58123d2c856cbe3f6
Andrew Erickson <aerickson@mozilla.com>
2023-09-09 03:11:46 -0700

    cleanup/fix post specs

--- test/integration/snakepit-head-post/inspec/users_spec.rb
+++ test/integration/snakepit-head-post/inspec/users_spec.rb
@@ -1,14 +1,13 @@
-describe "users" do
-    describe user("root") do
-      it { should exist }
-    end
-    describe user("slurm") do
-      it { should exist }
-      its("uid") { should eq 1877 }
-      its("gid") { should eq 1877 }
-      its("home") { should eq "/home/slurm" }
-      its("shell") { should eq "/bin/bash" }
-    end
+  describe user("root") do
+    it { should exist }
+  end
+
+describe user("slurm") do
+    it { should exist }
+    its("uid") { should eq 1877 }
+    its("gid") { should eq 1877 }
+    its("home") { should eq "/home/slurm" }
+    its("shell") { should eq "/bin/bash" }
   end
 
   # TODO: check for slurm user and correct uid/gid
