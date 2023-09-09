95de87679b8eccad87b731f6e2beb5ca69202c2f
Andrew Erickson <aerickson@mozilla.com>
2023-09-08 18:19:39 -0700

    add breaking user spec to head

--- test/integration/snakepit-head/inspec/users_spec.rb
+++ test/integration/snakepit-head/inspec/users_spec.rb
@@ -6,3 +6,13 @@ end
 
 # TODO: check for slurm user and correct uid/gid
 #       ... slurm group
+
+describe "users" do
+  describe user("aerickson") do
+    it { should exist }
+  end
+
+  describe user("epavlov") do
+    it { should exist }
+  end
+end
