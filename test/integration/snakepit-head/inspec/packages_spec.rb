2d9c757a1322d5c9c88b6cf5a7295abebadadef4
Andrew Erickson <aerickson@mozilla.com>
2023-09-11 11:35:37 -0700

    add mysql-server test

--- test/integration/snakepit-head/inspec/packages_spec.rb
+++ test/integration/snakepit-head/inspec/packages_spec.rb
@@ -1,7 +1,14 @@
-package_names = ["vim", "iputils-ping", "git", "wget", "tmux", "screen", "htop", "nano", "build-essential", "make"]
+# general packages
+package_names = ["vim", "iputils-ping", "git", "wget", "tmux", "screen",
+    "htop", "nano", "build-essential", "make"]
 
 package_names.each do |item|
   describe package(item) do
     it { should be_installed }
   end
+end
+
+# mysql server
+describe package("mysql-server") do
+  it { should be_installed }
 end
